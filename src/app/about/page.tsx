59bec38c415de5608e8d752ff73ccf5aee1cdd48
Sunday favour <90037865+Sunday012@users.noreply.github.com>
2024-03-05 00:34:21 +0100

    legacies added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,8 @@
 
+import React from "react";
 import About from "@/components/about/About";
 import Footer from "@/components/home/utils/Footer";
-import { Sora } from "next/font/google";
-import React from "react";
 
-const sora = Sora({
-  subsets: ["latin"],
-  weight: ["500"],
-});
 function Page() {
   return (
     <>

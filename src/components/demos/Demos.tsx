8ac4e539f7064ecefb3774cc9e02d134fd350660
rorado <150631441+rorado@users.noreply.github.com>
2024-03-15 17:19:10 +0000

    add animition

--- src/components/demos/Demos.tsx
+++ src/components/demos/Demos.tsx
@@ -1,20 +1,22 @@
-import styles from "./css/demos.module.css";
+"use client";
 
+import styles from "./css/demos.module.css";
 import Image from "next/image";
 import icone from "./img/icone_watch.svg";
-import image from "./img/video-im.png";
+
+import { motion } from "framer-motion";
 
 function Demos() {
   const cards = [
     { desc: "IT Data Center Services DDOS Cybersecurity Monitoring" },
-    { desc: "IT Data Center Services DDOS Cybersecurity Monitoring" },
-    { desc: "IT Data Center Services DDOS Cybersecurity Monitoring" },
-    { desc: "IT Data Center Services DDOS Cybersecurity Monitoring" },
-    { desc: "IT Data Center Services DDOS Cybersecurity Monitoring" },
-    { desc: "IT Data Center Services DDOS Cybersecurity Monitoring" },
-    { desc: "IT Data Center Services DDOS Cybersecurity Monitoring" },
-    { desc: "IT Data Center Services DDOS Cybersecurity Monitoring" },
-    { desc: "IT Data Center Services DDOS Cybersecurity Monitoring" },
+    { desc: "IT Internet Service DDOS Cybersecurity Monitoring" },
+    { desc: "IT Wireless Router Cybersecurity Monitoring" },
+    { desc: "IT Internet Service Monitoring" },
+    { desc: "IT Service Analytics" },
+    { desc: "IT Network Analytics" },
+    { desc: "IT Unknown Traffic Discovery" },
+    { desc: "IT Big Data Analytics" },
+    { desc: "IT Service Monitoring" },
   ];
 
   return (
@@ -25,13 +27,21 @@ function Demos() {
       </div>
       <div className={styles.centent}>
         {cards.map((card, i) => (
-          <div className={styles.card} key={i}>
+          <motion.div
+            className={styles.card}
+            key={i}
+            initial={{ x: i % 2 ? -90 : 0, y: -90, opacity: 0 }}
+            animate={{ y: 0, opacity: 1, x: 0 }}
+            transition={{
+              duration: 1,
+            }}
+          >
             <h3 className={styles.desc}>{card.desc}</h3>
             <div className={styles.card_watch}>
               <Image src={icone} alt="" width={16} height={18} />
               <h4>Watch Now</h4>
             </div>
-          </div>
+          </motion.div>
         ))}
       </div>
     </>

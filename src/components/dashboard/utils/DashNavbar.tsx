46a26cc16d53be087a6e3e21d0231f8b36d0b9e8
Kjazer004 <98173440+Kjazer004@users.noreply.github.com>
2024-03-17 21:26:09 +0100

    small fix

--- src/components/dashboard/utils/DashNavbar.tsx
+++ src/components/dashboard/utils/DashNavbar.tsx
@@ -6,8 +6,8 @@ import Image from "next/image";
 import Logo from "./img/Logo.png";
 import Size from "./img/Vector.png";
 import Profile from "./img/Account.png";
-import { Archivo, Sora } from "next/font/google";
-import { SetStateAction, useState } from "react";
+import { Archivo } from "next/font/google";
+import { useState } from "react";
 import Svgcomp from "./img/Svgcomp";
 
 const archivo = Archivo({
@@ -43,18 +43,36 @@ function DashboardNavbar() {
     setInd(index);
     setHover(hover);
   }
+  const [nav, setNav] = useState(false);
+
+  const variants = {
+    open: { width: "240px" },
+    closed: { width: "90px" },
+  };
+
+  const navButtons = {
+    width: nav ? "0px" : "max-content",
+    overflow: nav ? "hidden" : "show",
+    PointerEvent: nav ? "none" : "auto",
+    flexShrink: "0",
+    transition: "width 0.3s ease",
+  };
 
   return (
-    <div className={styles.Container}>
+    <motion.div
+      className={styles.Container}
+      animate={nav ? "closed" : "open"}
+      variants={variants}
+    >
       <div className={styles.Top}>
         <Image src={Logo} className={styles.LogoImage} alt={"Logo"} />
-        <div className={styles.ButtonWidth}>
+        <div className={styles.ButtonWidth} onClick={() => setNav(!nav)}>
           <Image src={Size} className={styles.SizeImage} alt={""} />
         </div>
       </div>
       <div className={styles.Account}>
         <Image src={Profile} alt={""} className={styles.AccountPic} />
-        <div className={styles.AccountInfo}>
+        <div className={styles.AccountInfo} style={navButtons}>
           <div className={styles.AccountName}>
             <p className={archivo.className}>Last Name</p>
           </div>
@@ -73,10 +91,12 @@ function DashboardNavbar() {
               onMouseEnter={() => hoverer(index, true)}
               onMouseLeave={() => hoverer(index, false)}
             >
-              <Svgcomp
-                type={`${bop.url}`}
-                fill={hover && ind == index ? "#9538fe" : "#000000"}
-              />
+              <div>
+                <Svgcomp
+                  type={`${bop.url}`}
+                  fill={hover && ind == index ? "#9538fe" : "#000000"}
+                />
+              </div>
               <div
                 className={styles.ButtonUrl}
                 style={{ color: hover && ind == index ? "#9538fe" : "#000000" }}
@@ -97,10 +117,12 @@ function DashboardNavbar() {
               onMouseEnter={() => hoverer(index + 100, true)}
               onMouseLeave={() => hoverer(index + 100, false)}
             >
-              <Svgcomp
-                type={`${bop.url}`}
-                fill={hover && ind == index + 100 ? "#9538fe" : "#000000"}
-              />
+              <div>
+                <Svgcomp
+                  type={`${bop.url}`}
+                  fill={hover && ind == index ? "#9538fe" : "#000000"}
+                />
+              </div>
               <div
                 className={styles.ButtonUrl}
                 style={{
@@ -113,7 +135,7 @@ function DashboardNavbar() {
           ))}
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
 

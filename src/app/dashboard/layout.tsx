f96a69a1d842e3fe34808733a0172ef9664df71c
Kjazer004 <98173440+Kjazer004@users.noreply.github.com>
2024-03-18 17:30:57 +0100

    Layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,17 +1,19 @@
 import DashboardNavbar from "@/components/dashboard/utils/DashNavbar";
-import React from "react";
-import styles from "@/components/dashboard/Main.module.css";
 import DashboardSearch from "@/components/dashboard/utils/DashboardSearch";
+import styles from "@/components/dashboard/Main.module.css";
 
-function Page() {
+export default function DashboardLayout({
+  children, // will be a page or nested layout
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <div className={styles.Container}>
       <DashboardNavbar />
       <div className={styles.MainContainer}>
         <DashboardSearch />
+        <div style={{ paddingLeft: "30px" }}>{children}</div>
       </div>
     </div>
   );
 }
-
-export default Page;

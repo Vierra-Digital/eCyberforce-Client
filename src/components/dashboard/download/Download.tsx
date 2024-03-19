2fe1f5bf1d7859dc276717913c7cc8e3a58882be
Sunday favour <90037865+Sunday012@users.noreply.github.com>
2024-03-19 04:11:25 +0100

    small fix

--- src/components/dashboard/download/Download.tsx
+++ src/components/dashboard/download/Download.tsx
@@ -1,78 +1,49 @@
-import { Archivo } from 'next/font/google'
-import Image from 'next/image'
-import React from 'react'
-import DownloadedProd from './_components/DownloadedProd'
-import styles from "../download/css/download.module.css"
+import { Archivo } from "next/font/google";
+import Image from "next/image";
+import React from "react";
+import DownloadedProd from "./_components/DownloadedProd";
+import styles from "../download/css/download.module.css";
 const archivo = Archivo({
-    subsets: ["latin"],
-    weight: ["400", "500"]
-})
-
-
+  subsets: ["latin"],
+  weight: ["400", "500"],
+});
 
 export default function Download() {
-    
   return (
     <div>
-        <div className='px-[24px]'>
-            <h1 className={`text-[#18181B] text-[32px] font-medium mt-6`}>Downloads</h1>
-            <div className='flex gap-[20px] justify-end my-6'>
-                <button className='flex gap-[10px] p-[10px] border border-[#EAE9F7] rounded-[4px] w-[150px] h-[65px] items-center justify-center'>
-                    <Image 
-                    src="/clock.svg"
-                    alt='clock'
-                    width={24}
-                    height={24}
-                    />
-                    <span className='text-nowrap'>Select Date</span>
-                </button>
-                <button className='flex gap-[10px] p-[10px] border border-[#EAE9F7] rounded-[4px] w-[150px] h-[65px] items-center justify-center'>
-                    <Image 
-                    src="/funnel.svg"
-                    alt='clock'
-                    width={24}
-                    height={24}
-                    />
-                    <span>Filters</span>
-                </button>
-            </div>
-
-            <div className='flex items-center justify-between'>
-                <div className='flex'>
-                  <div className='flex gap-2 w-[390px]'>
-                   <h4>Product</h4>
-                   <Image 
-                   src="/dropdown.svg"
-                   alt='drop'
-                   width={6}
-                   height={3}
-                   />
-                  </div>
-                  <div className='flex gap-2'>
-                  <h4>Expiry</h4>
-                   <Image 
-                   src="/dropdown.svg"
-                   alt='drop'
-                   width={6}
-                   height={3}
-                   />
-                  </div>
-                </div>
-                <div className='flex gap-2'>
-                  <h4>Action</h4>
-                   <Image 
-                   src="/dropdown.svg"
-                   alt='drop'
-                   width={6}
-                   height={3}
-                   />
-                </div>
-            </div>
-            <DownloadedProd />
-            
+      <div className="px-[24px]">
+        <h1 className={`text-[#18181B] text-[32px] font-medium mt-6`}>
+          Downloads
+        </h1>
+        <div className="flex gap-[20px] lg:justify-end justify-start my-6">
+          <button className="flex gap-[10px] p-[10px] border border-[#EAE9F7] rounded-[4px] w-[150px] h-[65px] items-center justify-center">
+            <Image src="/clock.svg" alt="clock" width={24} height={24} />
+            <span className="text-nowrap">Select Date</span>
+          </button>
+          <button className="flex gap-[10px] p-[10px] border border-[#EAE9F7] rounded-[4px] w-[150px] h-[65px] items-center justify-center">
+            <Image src="/funnel.svg" alt="clock" width={24} height={24} />
+            <span>Filters</span>
+          </button>
         </div>
+
+        <div className="flex items-center justify-between">
+          <div className="flex">
+            <div className="flex gap-2 w-[390px]">
+              <h4>Product</h4>
+              <Image src="/dropdown.svg" alt="drop" width={6} height={3} />
+            </div>
+            <div className="flex gap-2">
+              <h4>Expiry</h4>
+              <Image src="/dropdown.svg" alt="drop" width={6} height={3} />
+            </div>
+          </div>
+          <div className="flex gap-2">
+            <h4>Action</h4>
+            <Image src="/dropdown.svg" alt="drop" width={6} height={3} />
+          </div>
+        </div>
+        <DownloadedProd />
+      </div>
     </div>
-  )
+  );
 }
-
-
---
+++ src/components/dashboard/download/_components/DownloadProdItem.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+type Dprops = {
+  name: string;
+  date: string;
+};
+
+import styles from "../css/download.module.css";
+export default function DownloadProdItem({ name, date }: Dprops) {
+  const backgroundColor =
+    date === "Subscribe (January 5,2024)"
+      ? "rounded-[4px] bg-black text-white p-[10px] w-[163px] h-[48px]"
+      : "border border-[#EAE9F7] rounded-[1px] p-[10px] w-[163px] h-[48px]";
+  const text = date === "Subscribe (January 5,2024)" ? "Renew" : "View Details";
+  return (
+    <div
+      className={`flex items-center justify-between rounded-[6px] py-[4px] hover:bg-[#F7F8FA] w-full`}
+    >
+      <div className="flex">
+        <div className="flex gap-2 w-[390px]">
+          <h4>{name}</h4>
+        </div>
+        <div className="flex gap-2">
+          <h4>{date}</h4>
+        </div>
+      </div>
+      <div className="flex gap-6">
+        <button className={backgroundColor}>{text}</button>
+        <button className={styles.button}>Download Now</button>
+      </div>
+    </div>
+  );
+}

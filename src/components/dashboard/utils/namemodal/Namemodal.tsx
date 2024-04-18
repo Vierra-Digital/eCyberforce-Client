56a550e6b19523741abf2aa1e4ad0d233f0d6bd3
Kjazer004 <98173440+Kjazer004@users.noreply.github.com>
2024-04-18 18:11:17 +0200

    .

---
+++ src/components/dashboard/utils/namemodal/Namemodal.tsx
@@ -0,0 +1,13 @@
+"use server";
+
+import styles from "./Namemodal.module.css";
+
+function Namemodal() {
+  return (
+    <div className={styles.Container}>
+      <div></div>
+    </div>
+  );
+}
+
+export default Namemodal;
